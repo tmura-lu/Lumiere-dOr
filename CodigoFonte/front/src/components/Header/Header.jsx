77151bc4bb413e3275f91be51f2606a6c29b416d
Gabriel Moraes <moraesgabriel0003@gmail.com>
2025-06-20 17:50:05 -0300

    feat: implementação componente header

---
+++ CodigoFonte/front/src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'; 
+import { LuUser } from "react-icons/lu";
+import { FiShoppingBag } from "react-icons/fi";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.containerTitleIcon}>
+            <h1><Link className={styles.titulo}>Lumière d'Or</Link></h1>
+            
+            <div className={styles.containerIcon}>
+                <Link className={styles.icon}><LuUser /></Link>
+                <Link className={styles.icon}><FiShoppingBag/></Link>
+            </div>
+        </div>
+
+        <ul>
+            <li><Link className={styles.link}>Alianças</Link></li>
+            <li><Link className={styles.link}>Correntes</Link></li>
+            <li><Link className={styles.link}>Relógios</Link></li>
+            <li><Link className={styles.link}>Pulseiras</Link></li>
+            <li><Link className={styles.link}>Óculos</Link></li>
+            <li><Link className={styles.link}>Brincos</Link></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Home
---
+++ CodigoFonte/front/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import styles from './Home.module.css' 
+import Header from '../../components/Header/Header'
+
+function Home() {
+  return (
+    <div className={styles.container}>
+        <Header/>
+
+        
+    </div>
+  )
+}
+
+export default Home
