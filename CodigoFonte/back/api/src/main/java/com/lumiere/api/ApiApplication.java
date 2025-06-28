64de7a5e41dadb675c9f8fede5ca4b0b48c65a6b
Heitor Ramos <heitorvieira1220@gmail.com>
2025-06-27 22:10:47 -0300

    Criptografia de senha

--- CodigoFonte/back/api/src/main/java/com/lumiere/api/ApiApplication.java
+++ CodigoFonte/back/api/src/main/java/com/lumiere/api/ApiApplication.java
@@ -2,6 +2,8 @@ package com.lumiere.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +11,8 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	@Bean // <--- Adicione este método aqui
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
